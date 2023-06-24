d5497c1fa5491f9e6a66c0059b159d7c8a7032fb
Habib <shahin.amla@gmail.com>
2023-06-25 00:11:58 +0530

    Changed in navbar

--- harry-react-lesson/src/Navbar.js
+++ harry-react-lesson/src/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = ({searchbar}) => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                
                 <div className="collapse navbar-collapse">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
